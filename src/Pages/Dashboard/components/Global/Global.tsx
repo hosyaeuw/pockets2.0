2abe570935ccda7a7893a1bfc072eab39945cb3d
hosyaeuw <buyclock@mail.ru>
2022-08-16 17:20:47 +0300

    feat: create global info in dashboard

--- src/Pages/Dashboard/components/Global/Global.tsx
+++ src/Pages/Dashboard/components/Global/Global.tsx
@@ -1,7 +1,39 @@
+import React from "react";
 import { Content, Text } from "../../../../components";
 import { GlobalPlates } from "../../../common/components";
+import useCategories from "../../../common/hooks/useCategories";
 
 import styles from "./Global.module.scss";
+
+// TODO: вынести
+const GlobalGoals = () => {
+    return (
+        <div className={styles["global-goals"]}>
+            <Text size="s" color="secondary">
+                Отложенно на цели
+            </Text>
+            <Text size="s">36000</Text>
+        </div>
+    );
+};
+
+const MoreExtends = () => {
+    const { items } = useCategories();
+
+    const moreExtendsCategory = React.useMemo(
+        () => [...items].sort((a, b) => b.amount - a.amount)[0],
+        [items]
+    );
+
+    return (
+        <div className={styles["global-goals"]}>
+            <Text size="s" color="secondary">
+                Больше всего расходов
+            </Text>
+            <Text size="s" oneLine>{moreExtendsCategory.name}</Text>
+        </div>
+    );
+};
 
 const Global = () => {
     return (
@@ -19,7 +51,13 @@ const Global = () => {
                 </div>
             }
         >
-            <GlobalPlates />
+            <div className={styles.global__plates}>
+                <GlobalPlates />
+            </div>
+            <div className={styles.global__info}>
+                <GlobalGoals />
+                <MoreExtends />
+            </div>
         </Content>
     );
 };
