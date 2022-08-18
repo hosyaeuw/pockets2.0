10193121f4c9374f36bbc54e123ba85b2639b077
hosyaeuw <buyclock@mail.ru>
2022-08-18 09:00:46 +0300

    feat: add redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 import { store } from "./redux/store";
 
 import "./styles/globalStyles.scss";
+
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
     <Provider store={store}>
-        <Router>
-            <App />
-        </Router>
+        <PersistGate loading={null} persistor={persistor}>
+            <Router>
+                <App />
+            </Router>
+        </PersistGate>
     </Provider>
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,52 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import categories from "./slices/categories";
 import transactions from "./slices/transactions";
 import globalTransactions from "./slices/globalTransactions";
 import goals from "./slices/goals";
+import storage from "redux-persist/lib/storage";
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+
+const reducersObj = {
+    categories,
+    transactions,
+    globalTransactions,
+    goals,
+};
+
+const reducers = combineReducers(reducersObj);
+
+const persistConfig = {
+    key: "@app",
+    storage,
+    whitelist: Object.keys(reducersObj),
+    version: 0,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-    reducer: { categories, transactions, globalTransactions, goals },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
